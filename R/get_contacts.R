d261c5816a44d89356cdb9ec057bb0f8075f56fa
your_username <your_email@example.com>
2015-02-18 17:50:23 -0500

    initial commit

---
+++ R/get_contacts.R
@@ -0,0 +1,11 @@
+#' get_contacts()
+#'
+#' This function returns the number of contacts a participant had with LAYC services.
+#' @param df dataframe: the name of the dataframe containing the information.
+#' @return data frame
+#' @export
+#' @examples
+#' get_contacts(df = my_data)
+get_contacts <- function(df) {
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+## laycDosage package
+
+# What it does:
+Provide a set of functions to easily get dosage statistics and create plots
